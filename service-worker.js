f5694ccc7e0f81fdcbb8bd99a1c07ae88402f769
C.Silva <csdesweb@gmail.com>
2026-01-07 14:40:13 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,20 @@
-const CACHE = "moneyzen-v1";
-const FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
-];
+const CACHE = 'moneyzen-cs-v1';
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json'
+      ])
+    )
+  );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
