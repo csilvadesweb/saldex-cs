63e9ac94437ac16e06eb64433594ebc20d269f6e
C.Silva <csdesweb@gmail.com>
2026-01-09 17:31:39 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,28 @@
-const CACHE_NAME = 'saldex-cs-enterprise-v1';
+const CACHE_NAME = 'saldex-cs-enterprise-v2';
 
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './manifest.json'
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
   );
 });
