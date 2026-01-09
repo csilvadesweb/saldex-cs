430faf530ba7f83f38ef41eb57a9a94e2df86398
C.Silva <csdesweb@gmail.com>
2026-01-09 13:15:30 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,10 @@
-const CACHE_NAME = "moneyzen-cs-v1";
+const CACHE_NAME = 'moneyzen-v1';
+const assets = ['/', '/index.html', '/style.css', '/script.js', '/Icon-192.png'];
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./app.js",
-  "./manifest.json",
-  "./privacy.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
