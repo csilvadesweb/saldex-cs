005c8b488a34e0b95f177fd0c3cff572a66d0423
C.Silva <csdesweb@gmail.com>
2026-01-09 17:02:19 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'saldex-cache-v1';
+const CACHE_NAME = 'saldex-cache-v2';
 
 const ASSETS_TO_CACHE = [
   './',
@@ -7,41 +7,23 @@ const ASSETS_TO_CACHE = [
   './script.js',
   './manifest.json',
   './privacy.html',
-
-  // Ícones locais
-  './icons/icon-72.png',
-  './icons/icon-96.png',
-  './icons/icon-128.png',
-  './icons/icon-144.png',
-  './icons/icon-152.png',
-  './icons/icon-192.png',
-  './icons/icon-384.png',
-  './icons/icon-512.png',
-
-  // Bibliotecas externas
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
-// Instalação
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('SALDEX CS: Cache criado');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+self.addEventListener('install', (event) => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-// Ativação (remove caches antigos)
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then((keys) =>
       Promise.all(
-        keys.map(key => {
+        keys.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('SALDEX CS: Cache antigo removido', key);
             return caches.delete(key);
           }
         })
@@ -51,10 +33,9 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// Interceptação de requisições
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
